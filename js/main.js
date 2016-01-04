c63e7a6e6e0e66cd4832cf09e37db01627654ccb
Andrzej Kopeć <kapkowski@gmail.com>
2016-01-04 20:19:47 +0100

    [1.3] Promoted products in Vanilla JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 (function () {
     'use strict';
 
+    var promotedProducts = [
+        {name: 'Latte', price: '$10'},
+        {name: 'Green Tea', price: '$7'}
+    ];
+
     var products = [
         {name: 'Coffee', price: '$5'},
         {name: 'Tea', price: '$4'},
         {name: 'Yerba', price: '$6'}
     ];
 
-    products.forEach(function (product) {
-        var productComponent = new ProductComponent(product);
-        document.body.appendChild(productComponent.getDOMElement());
-    });
+    var promotedProductList = new ProductList(promotedProducts);
+    document.body.appendChild(promotedProductList.getDOMElement());
+
+    var productList = new ProductList(products);
+    document.body.appendChild(productList.getDOMElement());
 })();
--- js/product.js
+++ js/product.js
@@ -24,3 +24,4 @@ ProductComponent.prototype._createElement = function () {
     return div;
 };
 
+
