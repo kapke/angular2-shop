107783a0e5beeb57d895fdb4a9bdb8d802a453db
Andrzej Kopeć <kapkowski@gmail.com>
2016-01-04 22:09:26 +0100

    [1.4] Refactor into classes and modules. Added system.js

--- app/product.js
+++ app/product.js
@@ -1,20 +1,35 @@
-'use strict';
-function ProductComponent(product) {
-    this._product = product;
-    this._element = this._createElement();
-}
-ProductComponent.prototype.getDOMElement = function () {
-    return this._element;
-};
-ProductComponent.prototype._createElement = function () {
-    var div = document.createElement('div');
-    var title = document.createElement('header');
-    title.textContent = this._product.name;
-    var price = document.createElement('span');
-    price.className = 'price';
-    price.textContent = this._product.price;
-    div.appendChild(title);
-    div.appendChild(price);
-    return div;
-};
+System.register([], function(exports_1) {
+    var ProductComponent;
+    return {
+        setters:[],
+        execute: function() {
+            ProductComponent = (function () {
+                function ProductComponent(product) {
+                    this.product = product;
+                    this.element = this.createElement();
+                }
+                Object.defineProperty(ProductComponent.prototype, "domElement", {
+                    get: function () {
+                        return this.element;
+                    },
+                    enumerable: true,
+                    configurable: true
+                });
+                ProductComponent.prototype.createElement = function () {
+                    var div = document.createElement('div');
+                    var title = document.createElement('header');
+                    title.textContent = this.product.name;
+                    var price = document.createElement('span');
+                    price.className = 'price';
+                    price.textContent = this.product.price;
+                    div.appendChild(title);
+                    div.appendChild(price);
+                    return div;
+                };
+                return ProductComponent;
+            })();
+            exports_1("default", ProductComponent);
+        }
+    }
+});
 //# sourceMappingURL=product.js.map
---
+++ system.config.js
@@ -0,0 +1,4 @@
+System.config({
+    baseURL: "./",
+    defaultJSExtensions: true
+});
