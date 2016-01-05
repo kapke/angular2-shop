5e3d8335cfd91eb531b1a4ef6c4fe6572fb60340
Andrzej Kopeć <kapkowski@gmail.com>
2016-01-05 20:07:52 +0100

    [2.2] Added filtering

--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -9,15 +9,45 @@ import Product from "../../entities/Product";
     templateUrl: 'dist/components/App/my-app.html'
 })
 export default class App {
-    public title:string = 'Shop';
-    public products : Product[] = [
-        new Product('Coffee', '$5'),
-        new Product('Tea', '$4', ['Great', 'Super']),
-        new Product('Yerba', '$4', ['Strong'])
-    ];
-    public promotedProducts : Product[] = [
-        new Product('Latte', '$10'),
-        new Product('Green Tea', '$7')
-    ];
+    private filter: string = '';
+    public title: string = 'Shop';
+    public products: Product[] = [];
+    public promotedProducts: Product[] = [];
 
+    constructor () {
+        this.updateProducts();
+    }
+
+    public onFilterChange (newFilter: string) {
+        this.filter = newFilter;
+        this.updateProducts();
+    }
+
+    private updateProducts () {
+        this.products = this.getProducts().filter(this.filterProducts);
+        this.promotedProducts = this.getPromotedProducts().filter(this.filterProducts);
+    }
+
+    private filterProducts = (product: Product) => {
+        if(!this.filter.length) {
+            return true;
+        } else {
+            return product.name.indexOf(this.filter) != -1;
+        }
+    };
+
+    private getProducts () {
+        return [
+            new Product('Coffee', '$5'),
+            new Product('Tea', '$4', ['Great', 'Super']),
+            new Product('Yerba', '$4', ['Strong'])
+        ];
+    }
+
+    private getPromotedProducts () {
+        return [
+            new Product('Latte', '$10'),
+            new Product('Green Tea', '$7')
+        ];
+    }
 }
