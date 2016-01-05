701f439c67314caa324bc4cb4d5fa69d649185d2
Andrzej Kopeć <kapkowski@gmail.com>
2016-01-05 17:01:51 +0100

    [1.6] Displayed list of products in ng2

--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -1,9 +1,11 @@
 import {Component, View} from 'angular2/core';
+import ProductList from '../ProductList/ProductList';
 
 @Component({
     selector: 'my-app',
+    directives: [ProductList],
     templateUrl: 'dist/components/App/my-app.html'
 })
 export default class App {
-    public hello:string = 'Hello, World!';
+    public title:string = 'Shop';
 }
