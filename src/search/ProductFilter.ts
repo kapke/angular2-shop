64d9a63ad4a5a1b61d971dd6d551157dd011707c
Andrzej Kopeć <kapkowski@gmail.com>
2016-01-06 02:11:17 +0100

    [2.4] Moved filtering into pipe

---
+++ src/search/ProductFilter.ts
@@ -0,0 +1,19 @@
+import {Pipe} from 'angular2/core';
+import Product from "../entities/Product";
+
+@Pipe({
+    name: 'productFilter'
+})
+export default class ProductFilter {
+    public transform (items, [filterText]) {
+        return items.filter(this.filterProducts.bind(null, filterText));
+    }
+
+    private filterProducts (filterText: string, product: Product) {
+        if(!filterText.length) {
+            return true;
+        } else {
+            return product.toString().toLocaleLowerCase().indexOf(filterText.toLocaleLowerCase()) != -1;
+        }
+    };
+}
