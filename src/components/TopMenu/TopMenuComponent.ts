f103edcfbf0d8321c722e060a4c718cd00ab0f07
Andrzej Kopeć <kapkowski@gmail.com>
2016-01-08 22:15:28 +0100

    [3.3] Added active class for active item in menu

--- src/components/TopMenu/TopMenuComponent.ts
+++ src/components/TopMenu/TopMenuComponent.ts
@@ -1,6 +1,6 @@
 import {Component} from 'angular2/core';
 import {COMMON_DIRECTIVES} from 'angular2/common';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {ROUTER_DIRECTIVES, Router} from 'angular2/router';
 
 @Component({
     selector: 'top-menu',
@@ -9,7 +9,7 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
     template: `
         <nav>
             <ul class="nav nav-tabs">
-                <li *ngFor="#item of items">
+                <li *ngFor="#item of items" [ngClass]="{active: isLinkActive(item.link)}">
                     <a [routerLink]="[item.link]">{{ item.title }}</a>
                 </li>
             </ul>
@@ -18,9 +18,19 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
     styles: [
         `ul {
             margin-bottom: 1em;
+        },
+        .router-link-active {
+
         }`
     ]
 })
 export default class TopMenuComponent {
     public items:{link: string; title: string}[] = []
+
+    constructor(private router: Router) {
+    }
+
+    public isLinkActive (link: string) {
+        return this.router.isRouteActive(this.router.generate([link]));
+    }
 }
