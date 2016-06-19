fcecd3b693609eb468677436d577a7393a984e49
Andrzej Kopeć <kapkowski@gmail.com>
2016-06-19 14:08:25 +0200

    [3.2] Display single product data

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 's-app',
   template: `
-    <h1>Hello, world!</h1>
+    <div>
+      <header>{{ product.name }}</header>
+      <span>Price: \${{ product.price }}</span>
+    </div>
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  public product: {name: string, price: number} = {
+    name: 'Coffee',
+    price: 10
+  };
+}
